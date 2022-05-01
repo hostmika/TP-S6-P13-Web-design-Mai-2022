579dc8ae0dfe0e202fed8ad15a807f5d4dd0c3c0
Michael <mika.rakotondraibe@gmail.com>
2022-05-01 23:35:10 +0300

    utilisation de layout yield

--- resources/views/pages/about-us.blade.php
+++ resources/views/pages/about-us.blade.php
@@ -1,25 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('template')
 
-        <title>Test laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        
-    </head>
-    <body>
+@section('content')
        <h1>Test Laravel fotsiny</h1>
 
        <p>&hearts;build by mika</p>
        <p> <a href="/"> Home </a> </p>
-
-       <footer>
-           <p>&copy;Copyrigth {{ date('Y') }} by mika &middot;<a href="about-us">About us </a> </p>
-       </footer>
-
-    </body>
-</html>
+@endsection
