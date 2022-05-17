6b6a8343b482d71ebee4aec16baafd8f8970a1c1
RAKOTONDRAIBE Michael <mika.rakotondraibe@gmail.com>
2022-05-17 14:49:02 +0300

    ajout de la template

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::name('home')->get('/', function () {
-    return view('pages/home');
+    return view('pages.contenu');
 });
 
-Route::get('/about-us', function () {
-    return view('pages/about-us');
-})->name('about');
+
